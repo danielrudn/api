ceb9fae3eb03fe185ada55753bbac011e8344708
Daniel <danielrudn@gmail.com>
2017-11-17 22:33:57 -0500

    Rooms now keep a history of tracks played

---
+++ src/v1/services/history-service.js
@@ -0,0 +1,27 @@
+import RedisService from './redis-service';
+import EventService from './event-service';
+import events from '../events/events';
+
+class HistoryService {
+  async getHistoryLength(room) {
+    return await RedisService.llen(`rooms:${room.id}:history`);
+  }
+
+  async getHistory(room) {
+    try {
+      return await RedisService.lrange(`rooms:${room.id}:history`);
+    } catch (err) {
+      return [];
+    }
+  }
+
+  async addTrack(room, track) {
+    await RedisService.lpush(`rooms:${room.id}:history`, track);
+    EventService.emit(events.ROOM_HISTORY_UPDATE, {
+      room,
+      history: await getHistory(room)
+    });
+  }
+}
+
+export default new HistoryService();
